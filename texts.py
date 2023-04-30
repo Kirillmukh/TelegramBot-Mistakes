b4406160fffa640e2c9192e00c3dd21c2d06cab5
Kirillmukh <109591294+Kirillmukh@users.noreply.github.com>
2023-04-30 23:13:21 +0300

    texts 0

---
+++ texts.py
@@ -0,0 +1,35 @@
+start = "Привет, я бот, который поможет тебе подготовиться к егэ"
+
+cancel = "\n/cancel - отмена"
+cancel_accept = "Отменено"
+
+input_step_1 = "Введите категорию" + cancel
+input_step_2 = "Введите слово или вопрос" + cancel
+input_step_3 = "Введите ответ" + cancel
+input_step_4 = "Хотите добавить описание?" + cancel
+input_step_5 = "Введите описание" + cancel
+input_step_6 = "Все успешно введено"
+
+delete_step_1 = "Введите категорию" + cancel
+delete_step_1_except = "Такой категории нет, введите категорию корректно" + cancel
+delete_step_2 = "Введите слово или вопрос" + cancel
+delete_step_2_except = "Такого слова нет в категории, введите повторно" + cancel
+delete_step3_1 = 'Слово "'
+delete_step3_2 = '" удалено'
+
+game_step_1 = "Введите категорию" + cancel
+game_step_1_except = "Такой категории нет, введите категорию корректно" + cancel
+input_words = "Сперва введите слова, /input"
+game_started = "Игра началась, категория "
+game_ended = "Игра закончена"
+game_accept = "Верно"
+game_except = "Неверно"
+game_reset_list_1 = 'Слова из категории "'
+game_reset_list_2 = '" обновлены'
+
+empty_list = "Cписок пуст"
+
+markup_add_another = "Добавить еще"
+markup_delete = "Убрать историю"
+
+not_in_use = "Я не понимаю, может / команда поможет мне)"
